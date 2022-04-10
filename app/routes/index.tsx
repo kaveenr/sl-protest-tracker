37eb988dcbab2fadebdb8186376fecfa04389950
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2022-04-10 08:50:44 +0530

    ui changes.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -27,6 +27,8 @@ export const loader: LoaderFunction = async ({request}) => {
     dataset: dataset.filter((i) => {
       if (from) {
         return i.date.startOf("day") <= from.startOf("day")
+      } else {
+        return true;
       }
     }),
     startDate: dataset[0].date,
@@ -40,9 +42,16 @@ const EmbedViewer = (props: {links: string[]}) =>  {
   }
   return (
     <div className='absolute top-0 right-0 p-4 z-50 w-96 overflow-y-auto max-h-full hidden md:block'>
+        <div className="card card-compact bg-base-100 shadow-xl">
+          <div className="card-body">
+            <p className='text-xs md:text-lg text-center'>
+              🔗 <b>{props.links.length}</b> Social Links
+            </p>
+          </div>
+        </div>
         {props.links.map((link) => (
           <div className="p-2" key={link}>
-            <Embed url={link} fallback={<h1>Loading data...</h1>}/>
+            <Embed url={link}/>
           </div>
         ))}
     </div>
@@ -79,15 +88,21 @@ export default function Index() {
             <h2 className="card-title text-md md:text-3xl">🇱🇰 Protest Tracker</h2>
             <p className='text-xs md:text-lg'>Visualization of protests taking place in Sri Lanka with data provided by <a href="https://www.watchdog.team" className='text-blue-100'>Watchdog</a>.</p>
             <div className="card-actions justify-left mt-2">
-              <a className="btn btn-primary btn-sm" href="https://docs.google.com/spreadsheets/d/1yShvemHd_eNNAtC3pmxPs9B5RbGmfBUP1O6WGQ5Ycrg/edit#gid=0">Data Source</a>
+              <a className="btn btn-primary btn-sm" href="https://docs.google.com/spreadsheets/d/1yShvemHd_eNNAtC3pmxPs9B5RbGmfBUP1O6WGQ5Ycrg/edit#gid=0" target="_blank">
+                Data Source
+              </a>
+              <a className="btn btn-secondary btn-sm" href="https://github.com/kaveenr/sl-protest-tracker" target="_blank">
+                <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="inline-block h-5 w-5 fill-current md:h-6 md:w-6"><path d="M256,32C132.3,32,32,134.9,32,261.7c0,101.5,64.2,187.5,153.2,217.9a17.56,17.56,0,0,0,3.8.4c8.3,0,11.5-6.1,11.5-11.4,0-5.5-.2-19.9-.3-39.1a102.4,102.4,0,0,1-22.6,2.7c-43.1,0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1,1.4-14.1h.1c22.5,2,34.3,23.8,34.3,23.8,11.2,19.6,26.2,25.1,39.6,25.1a63,63,0,0,0,25.6-6c2-14.8,7.8-24.9,14.2-30.7-49.7-5.8-102-25.5-102-113.5,0-25.1,8.7-45.6,23-61.6-2.3-5.8-10-29.2,2.2-60.8a18.64,18.64,0,0,1,5-.5c8.1,0,26.4,3.1,56.6,24.1a208.21,208.21,0,0,1,112.2,0c30.2-21,48.5-24.1,56.6-24.1a18.64,18.64,0,0,1,5,.5c12.2,31.6,4.5,55,2.2,60.8,14.3,16.1,23,36.6,23,61.6,0,88.2-52.4,107.6-102.3,113.3,8,7.1,15.2,21.1,15.2,42.5,0,30.7-.3,55.5-.3,63,0,5.4,3.1,11.5,11.4,11.5a19.35,19.35,0,0,0,4-.4C415.9,449.2,480,363.1,480,261.7,480,134.9,379.7,32,256,32Z"></path></svg>
+              </a>
             </div>
           </div>
         </div>
-        <div className="card w-80 md:w-96 bg-base-100 shadow-xl mt-4 hidden md:block">
+        <div className="card card-compact w-80 md:w-96 bg-base-100 shadow-xl mt-4 hidden md:block">
           <div className="card-body">
             <input type="range" min="0" max={dateRange} value={days} onChange={(e) => {setDays(parseInt(e.target.value)); setCurrent(undefined)}} className="range"/>
             <p className='text-center'>
-              <b>{dateRangeStart.toFormat("DD")}</b> - <b>{dateRangeStart.plus({days: days}).toFormat("DD")}</b>
+              <b>{dateRangeStart.toFormat("DD")}</b> - <b>{dateRangeStart.plus({days: days}).toFormat("DD")}</b><br/>
+              <b>{data.dataset.length}</b> Results
             </p>
           </div>
         </div>
@@ -135,7 +150,7 @@ export default function Index() {
               <div className="card-body text-xs md:text-xl" style={{padding: "0.2em 0.3em"}}>
                 <h2 className="card-title">
                   <p className="truncate">{current.location}</p>
-                  <div className="badge badge-secondary">{DateTime.fromISO(current.date.toString()).toLocaleString()}</div>  
+                  <div className="badge badge-secondary">{DateTime.fromISO(current.date.toString()).toFormat("DD")}</div>  
                 </h2>
                 <div className="prose-sm prose-stone">
                   <p>{current.notes || "Notes Not Available"}</p>
